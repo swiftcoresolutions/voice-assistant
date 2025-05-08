f82ff33efbd1ca8deea9f2288f9d51c810a3d9dc
Jake Sharp <yythebest1@gmail.com>
2025-05-07 23:22:50 -0400

    Add static greeting playback route

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/voice", methods=["POST"])
+@app.route('/voice', methods=['POST'])
 def voice():
     resp = VoiceResponse()
-    resp.play(f"{os.getenv('RENDER_EXTERNAL_HOSTNAME')}/static/greeting.mp3")
+    resp.play(f"{os.environ['RENDER_EXTERNAL_HOSTNAME']}/static/greeting.mp3")
     return str(resp)
 
-@app.route("/static/<path:filename>")
-def serve_static(filename):
-    return send_from_directory("static", filename)
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory('static', filename)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
